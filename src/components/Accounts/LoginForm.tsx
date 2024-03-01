5607991e8862cff5b7d714f589084dfddb08fd8b
V. Andre Washington <kuraiokamiblaze@gmail.com>
2024-02-29 22:45:10 -0700

    Import and edit login form component

---
+++ src/components/Accounts/LoginForm.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { useApp } from "../../provider/context-hooks";
+import { toast } from "react-hot-toast";
+import TextInput from "../shared/TextInput";
+import PasswordInput from "../shared/PasswordInput";
+
+function LoginForm() {
+  // const { users, loginActiveUser } = useUser();
+  const [userId, setUserId] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const disabled = !userId || !password;
+
+  const handleShowPassword = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setShowPassword(!showPassword);
+  };
+
+  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   const user = users.find((user) => user.userId === userId);
+
+  //   if (!user) {
+  //     toast.error("User not found!");
+  //     return;
+  //   } else if (user.password !== password) {
+  //     toast.error("Password is incorrect!");
+  //     return;
+  //   } else {
+  //     loginActiveUser(user);
+  //     setUserId("");
+  //     setPassword("");
+  //     toast.success(`Welcome back, ${user.userId}!`);
+  //   }
+  // };
+
+  return (
+    <form 
+      className="form login" 
+      // onSubmit={handleSubmit}
+    >
+      <header>
+        <h2 className="title">Welcome Back!</h2>
+        <h3 className="subtitle">
+          Sign in to your account to see all of your favorites and contribute to
+          the community!
+        </h3>
+      </header>
+
+      <div className="inputs-group">
+        <TextInput
+          label="Username"
+          type="text"
+          id="username"
+          value={userId}
+          textChange={(e) => setUserId(e.target.value)}
+        />
+
+        <PasswordInput
+          label="Password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          showPassword={showPassword}
+          setShowPassword={handleShowPassword}
+        />
+      </div>
+
+      <button type="submit" className="submit-button" disabled={disabled}>
+        Login
+      </button>
+    </form>
+  );
+}
+
+export default LoginForm;
